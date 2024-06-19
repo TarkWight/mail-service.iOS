8a9c9cd3105ee9904ac02b6f717cbe238db4dab6
Arsentiy Gonda <arsentiy.gonda@gmail.com>
2024-06-19 13:17:14 +0700

    [init] Initiated new project
    -- Add project with core data

---
+++ mail-service.iOS/mainApplication.swift
@@ -0,0 +1,20 @@
+//
+//  mainApplication.swift
+//  mail-service.iOS
+//
+//  Created by Tark Wight on 19.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct mainApplication: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
