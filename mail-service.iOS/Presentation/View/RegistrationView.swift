fa402fbd099491a23466ae5686f130864fd9226f
Arsentiy Gonda <arsentiy.gonda@gmail.com>
2024-06-23 13:22:24 +0700

    [init] Added registration view and viewModel

---
+++ mail-service.iOS/Presentation/View/RegistrationView.swift
@@ -0,0 +1,18 @@
+//
+//  RegistrationView.swift
+//  mail-service.iOS
+//
+//  Created by HITSStudent on 23.06.2024.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegistrationView()
+}
---
+++ mail-service.iOS/Presentation/ViewModel/RegistrationViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  RegistrationViewModel.swift
+//  mail-service.iOS
+//
+//  Created by HITSStudent on 23.06.2024.
+//
+
+import SwiftUI
+import Combine
+
+final class RegistrationViewModel: ObservableObject {
+    
+    
+    
+}
